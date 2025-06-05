a811cba6aed3132a42d03feaa67170f6980c04a8
DeepakSaiVarmaPenmatsa <pdsvarma4@gmail.com>
2025-06-05 09:42:41 +0530

    d

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-import express from 'express';
-const app=express();
-app.listen(8080, () => {
-    console.log("Server Started");
-});
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from 'dotenv';
+import userRouter from "./routes/userRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
 
-app.get("/",(req,res) => {
-    return res.send("Hello World");
-})
+dotenv.config();
 
-app.get("/greet",(req,res) => {
-    res.send("Greetings");
-});
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get("/name",(req,res) => {
-    res.send("Deepak");
-})
+const DBUSER = encodeURIComponent(process.env.DBUSER)
+const DBPASS = encodeURIComponent(process.env.DBPASS)
+const MONGO_URI =`mongodb+srv://${DBUSER}:${DBPASS}@cluster0.fsxnlxz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
-app.get("/weather", (req,res) => {
-    res.send("41Degrees");
-})
+// const MONGO_URI = process.env.MONGO_URI
+//testing
+app.use("/users", userRouter);
+app.use("/products", productRouter);
+app.use("/orders",orderRouter)
 
-app.get("/products", (req, res) => {
-  const products = [
-    { name: "Product 1", price: 34 },
-    { name: "Product 2", price: 64 },
-    { name: "Product 3", price: 45 },
-  ];
-  res.json(products);
-});
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server Started on port 8080");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
